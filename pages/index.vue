2d46533cf8f32a09399d619242ad9611eebbf742
TajamHeng <oojinheng@gmail.com>
2020-06-18 02:39:52 +0800

    placeholder pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,31 @@
 <template>
   <div>
+    <b-row align-h="center">
+      <b-col cols="3">
+        <b-img class="thumbnail" center fluid :src="picture.url" :alt="picture.name" />
+      </b-col>
+      <b-col cols="9" />
+    </b-row>
   </div>
 </template>
 
 <script>
 export default {
-  components: {
+  data: () => {
+    return {
+      picture: {
+        url: 'profile-picture.jpg',
+        name: 'Profile Image'
+      }
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
+  .thumbnail {
+    padding: .5em;
+    background-color: rgba(255, 255, 255, .75);
+    border-radius: .25em;
+  }
 </style>
---
+++ pages/project.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Nothing here yet!
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
