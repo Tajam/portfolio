3505e452d2c2f5210352292110f2ca5801361f81
TajamHeng <oojinheng@gmail.com>
2020-06-20 22:48:21 +0800

    attempt to fix url

--- build.js
+++ build.js
@@ -5,11 +5,18 @@ const { exec } = require('child_process')
 console.log('Building pages...')
 exec('npm run build:gh-pages', () => {
   console.log('Renamed \'index.html\' to \'404.html\'!')
-  find.file('dist', (files) => {
+  const path = 'dist'
+  find.file(path, (files) => {
     const name = 'index.html'
+    const first = true
     files.filter(file => file.substring(file.length - name.length, file.length) === name)
       .forEach(file => {
-        fs.rename(file, `${file.substring(0, file.length - name.length)}404.html`, () => {})
+        if (first) {
+          fs.rename(file, `${file.substring(0, file.length - name.length)}404.html`, () => {})
+          first = false
+        } else {
+          fs.delete(file)
+        }
       })
   })
 })
